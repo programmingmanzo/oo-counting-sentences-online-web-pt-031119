705fde99f38d7e04c42e229aedde86b28f98ca52
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-03 05:30:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -32,12 +32,11 @@ class String
 
 #self = "one. two. three?"
   def count_sentences
-    i = 1
-    self.split(".", "?", "!")
-    self.each do |element|
-      i += 1 
+    #i = 1
+    self.split.count 
+    #self.each do |element|
+      #i += 1 
     binding.pry 
-    end 
-    i
+    #end 
   end
 end
