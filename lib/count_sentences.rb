ca50993559ce7058167502b8988b11284c5b7f89
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-03 06:04:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true 
+      true 
+    else 
+      false  
+      #binding.pry 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?") == true 
+      true 
+    else 
+      false
+      #binding.pry 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true 
+      true 
+    else 
+      false 
+      #binding.pry 
+    end 
   end
+  
+
+#self = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
 
   def count_sentences
-
+    if self.end_with?(".") == false  
+      self.split.count 
+    else 
+    self.split(",").count 
+    #binding.pry 
+    end
   end
 end
